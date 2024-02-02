a0bd156057ed4d390ab11079b8a6c5ff0d094c84
vaishnavi-rama <vramanujan@ucsd.edu>
2024-02-01 16:40:35 -0800

    Fixed bugs in both reverse methods in ArrayExample.java

--- ArrayTests.java
+++ ArrayTests.java
@@ -9,10 +9,22 @@ public class ArrayTests {
     assertArrayEquals(new int[]{ 3 }, input1);
 	}
 
+  @Test
+  public void testReverseInPlaceMult() {
+    int[] input1 = { 3, 2, 1 };
+    ArrayExamples.reverseInPlace(input1);
+    assertArrayEquals(new int[]{3, 2, 1}, input1);
+	}
 
   @Test
   public void testReversed() {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+  }
+
+  @Test
+  public void testReversedNonEmpty() {
+    int[] input1 = {1, 2, 3 };
+    assertArrayEquals(new int[]{3, 2, 1}, ArrayExamples.reversed(input1));
   }
 }
